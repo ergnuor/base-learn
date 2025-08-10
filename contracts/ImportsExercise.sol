eaf3a5639707a5779cff2a7f17cc7016e183e85d
Denis Tarasov <brokeneye@gmail.com>
2025-08-10 23:50:54 +0300

    ImportsExercise

---
+++ contracts/ImportsExercise.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+library SillyStringUtils {
+
+    struct Haiku {
+        string line1;
+        string line2;
+        string line3;
+    }
+
+    function shruggie(string memory _input) internal pure returns (string memory) {
+        return string.concat(_input, unicode" 🤷");
+    }
+}
+
+contract ImportsExercise {
+    using SillyStringUtils for SillyStringUtils.Haiku;
+
+    SillyStringUtils.Haiku public haiku;
+
+    function saveHaiku(string memory _line1, string memory _line2, string memory _line3) external {
+        haiku = SillyStringUtils.Haiku({
+            line1: _line1,
+            line2: _line2,
+            line3: _line3
+        });
+    }
+    
+    function getHaiku() external view returns (SillyStringUtils.Haiku memory) {
+        return haiku;
+    }
+
+    function shruggieHaiku() external view returns (SillyStringUtils.Haiku memory) {
+        return SillyStringUtils.Haiku({
+            line1: haiku.line1,
+            line2: haiku.line2,
+            line3: SillyStringUtils.shruggie(haiku.line3)
+        });
+    }
+}
